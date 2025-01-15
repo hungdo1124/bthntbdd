ff72e8115b5505924a7903e6a967fa3844a2b45e
hungdo1124 <hungdo1124@gmail.com>
2025-01-15 13:24:49 +0700

    Add files via upload

---
+++ bt 15.1/import React from 'react'.ts
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="square">
+        Hello, world
+      </div>
+    </div>
+  );
+}
+
+export default App;
